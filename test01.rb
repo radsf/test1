0f01be673844a72680c8ef7160e30811ac6b8846
TsukimoriYuki <Yuukitsuki0717@gmail.com>
2023-05-19 16:16:53 +0900

    ついかー－－

--- test01.rb
+++ test01.rb
@@ -6,3 +6,11 @@ Window.height = 480
 Window.loop do
   # ゲームの処理を記述する
 end
+
+player_image = Image.new(32, 32)  # プレイヤーの画像を作成
+
+player = Sprite.new(0, 0, player_image)  # プレイヤースプライトを作成
+
+Window.loop do
+  player.draw  # プレイヤーを描画する
+end
