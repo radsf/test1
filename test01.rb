5291597631ddea7a815669e1d1895b7e6f020827
s225048 <s225048@matsu.shimane-u.ac.jp>
2023-05-26 15:43:36 +0900

    エラーです

--- test01.rb
+++ test01.rb
@@ -3,33 +3,19 @@ require 'dxruby'
 Window.width = 640
 Window.height = 480
 
-Window.loop do
-  # ゲームの処理を記述する
-
-
 Window.bgcolor = [255, 128, 255, 255] # 追加
 ground_img = Image.new(640, 80, [255, 116, 80, 48]) # 追加
 ground_img.box_fill(0, 0, 640, 10, [255, 0, 128, 0]) # 追加
 
-Window.loop do
-  Window.draw(0, 400, ground_img) # 追加
-end
+Window.draw(0, 400, ground_img) # 追加
 
 player_image = Image.new(32, 32)  # プレイヤーの画像を作成
 
 player = Sprite.new(0, 0, player_image)  # プレイヤースプライトを作成
 
-Window.loop do
-  player.draw  # プレイヤーを描画する
-end
-
 player_image = Image.new(32, 32)  # プレイヤーの画像を作成
 
 player = Sprite.new(0, 0, player_image)  # プレイヤースプライトを作成
-
-Window.loop do
-  player.draw  # プレイヤーを描画する
-end
 
 bullet_image = Image.new(8, 8)  # 弾の画像を作成
 
@@ -46,17 +32,9 @@ Window.loop do
 
   # 弾と敵の当たり判定
   Sprite.check(bullets, enemies)
-end
-
-Window.loop do
-  # 敵の生成や移動処理を記述する
 
   Sprite.update(enemies)  # 敵の位置を更新する
 
-  # 弾と敵の当たり判定
-  Sprite.check(bullets, enemies)
-
   Sprite.draw(enemies)  # 敵を描画する
-end
 
 end
